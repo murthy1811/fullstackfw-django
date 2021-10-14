505a0b5d550abd803a4912a5a12311f81d189e75
Murthy Valavala <murthy.mailme@gmail.com>
2021-10-14 19:07:20 +0000

    todo app done

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
+from .models import Item
+
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name= 'Test Todo Item')
+        self.assertFalse(item.done)
 
 
-class TestDjango(TestCase):
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name= 'Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
+    
 
-    def test_this_thing_works(self):
-        self.assertEqual(1,1)
+
+
+        
 
